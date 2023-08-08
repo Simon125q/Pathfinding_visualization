5fa32e5b7e744e85cef750285ea17a6fa462f363
Simon125q <106029626+Simon125q@users.noreply.github.com>
2023-08-08 09:16:41 +0200

    implement BFS

--- pathfinding.py
+++ pathfinding.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
+from collections import deque
 RES = WIDTH, HEIGHT = (1200, 800)
-FPS = 60
+FPS = 100
 TILE = 30
 ROWS = HEIGHT // TILE
 COLS = WIDTH // TILE
@@ -14,12 +15,10 @@ class Game:
         self.clock = pygame.time.Clock()
         self.restart()
 
-        
     def restart(self):
-        self.stack = []
+        self.start_point = 0
+        self.end_point = 0
         self.start = None
-        self.end = None
-        self.endpoints = 0
         self.grid_cells = [[Cell(col, row, self) for row in range(ROWS)] for col in range(COLS)]
         #self.grid_cells = [[0]*COLS]*ROWS
         #for row in range(ROWS):
@@ -33,6 +32,12 @@ class Game:
             for cell in row:
                 cell.update(self.screen)"""
         
+    def clean_grid(self):
+        for row in self.grid_cells:
+            for cell in row:
+                if cell.state == 'visited':
+                    cell.state = 'not_visited'
+        
     def check_events(self):
         
         for event in pygame.event.get():
@@ -44,25 +49,64 @@ class Game:
                 if event.key == pygame.K_ESCAPE:
                     self.restart()
                 elif event.key == pygame.K_SPACE:
-                    self.DFS()
+                    self.BFS()
     
-    def DFS(self):
-        if self.endpoints != 2 and self.grid_cells[0][0].state != 'endpoint':
-            self.grid_cells[0][0].state = 'endpoint'
-            self.endpoints += 1
-        if self.endpoints != 2:
-            self.grid_cells[-1][-1].state = 'endpoint'
-            self.endpoints += 1
+    def define_endpoints(self):
+        if self.start_point == 0 and self.grid_cells[0][0].state != 'end':
+            self.grid_cells[0][0].state = 'start'
+            self.start_point = 1
+        elif self.start_point == 0 and self.grid_cells[-1][-1].state != 'end':
+            self.grid_cells[-1][-1].state = 'start'
+            self.start_point = 1
+        if self.end_point == 0 and self.grid_cells[-1][-1].state != 'start':
+            self.grid_cells[-1][-1].state = 'end'
+            self.end_point = 1
+        elif self.end_point == 0 and self.grid_cells[0][0].state != 'start':
+            self.grid_cells[0][0].state = 'end'
+            self.end_point = 1
+            
+    def find_start(self):
+        self.define_endpoints()
+        for row in self.grid_cells:
+            for cell in row:
+                if cell.state == 'start':
+                    self.start = cell
+                    return
+    
+    def BFS(self):
+        self.find_start()
+        self.clean_grid()
+        queue = deque()
+        queue.append(self.start)
         
+        while queue:
+            curr = queue.popleft()
+            if curr.state == 'end':
+                break
+            
+            if curr.x - 1 >= 0 and self.grid_cells[curr.x - 1][curr.y].state == 'not_visited':
+                queue.append(self.grid_cells[curr.x - 1][curr.y])
+                self.grid_cells[curr.x - 1][curr.y].state = 'visited'
+            if curr.y - 1 >= 0 and self.grid_cells[curr.x][curr.y - 1].state == 'not_visited':
+                queue.append(self.grid_cells[curr.x][curr.y - 1])
+                self.grid_cells[curr.x][curr.y - 1].state = 'visited'
+            if curr.x + 1 < COLS and self.grid_cells[curr.x + 1][curr.y].state == 'not_visited':
+                queue.append(self.grid_cells[curr.x + 1][curr.y])
+                self.grid_cells[curr.x + 1][curr.y].state = 'visited'
+            if curr.y + 1 < ROWS and self.grid_cells[curr.x][curr.y + 1].state == 'not_visited':
+                queue.append(self.grid_cells[curr.x][curr.y + 1])
+                
+            self.update()
+            
     def update(self):
+        self.check_events()
+        self.draw()
         pygame.display.flip()
         self.clock.tick(FPS)
         pygame.display.set_caption('Pathfinding algorithm')
         
     def run(self):
         while True:
-            self.check_events()
-            self.draw()
             self.update()
         
 class Cell:
@@ -84,7 +128,9 @@ class Cell:
             pygame.draw.rect(screen, pygame.Color('blue'), self.rect, border_radius = 7)
         elif self.state == 'wall':
             pygame.draw.rect(screen, pygame.Color('red'), self.rect, border_radius = 7)
-        elif self.state == 'endpoint':
+        elif self.state == 'start':
+            pygame.draw.rect(screen, pygame.Color('green'), self.rect, border_radius = 7)
+        elif self.state == 'end':
             pygame.draw.rect(screen, pygame.Color('gold'), self.rect, border_radius = 7)
             
     def check_click(self):
@@ -101,12 +147,18 @@ class Cell:
             if pygame.mouse.get_pressed()[2] and self.can_click:
                 self.can_click = False
                 self.click_time = pygame.time.get_ticks()
-                if self.state == 'not_visited' and self.game.endpoints < 2:
-                    self.state = 'endpoint'
-                    self.game.endpoints += 1
-                elif self.state == 'endpoint':
-                    self.state = 'not_visited'
-                    self.game.endpoints -= 1
+                if self.state == 'not_visited' and self.game.start_point == 0:
+                    self.state = 'start'
+                    self.game.start_point = 1
+                elif self.state == 'not_visited' and self.game.start_point == 1 and self.game.end_point == 0:
+                    self.state = 'end'
+                    self.game.end_point = 1
+                elif self.state == 'end' and self.game.end_point == 1:
+                    self.state= 'not_visited'
+                    self.game.end_point = 0
+                elif self.state == 'start' and self.game.start_point == 1:
+                    self.state= 'not_visited'
+                    self.game.start_point = 0
         
     def cooldown(self):
         current_time = pygame.time.get_ticks()
